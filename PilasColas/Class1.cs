475f9c1ecd336465552daff2fc66c1ea5290e425
Carlos Ortiz <hardisaakpp@hotmail.es>
2024-02-12 01:00:18 -0500

    Add project files.

---
+++ PilasColas/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PilasColas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Pilas
+            Stack<int> stack = new Stack<int>();
+
+            // Agregar elementos a la pila
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            // Eliminar y obtener el elemento superior
+            int topElement = stack.Pop();
+
+            // Obtener el elemento superior sin eliminarlo
+            int peekedElement = stack.Peek();
+
+
+            // Colas
+            Queue<int> queue = new Queue<int>();
+
+            // Agregar elementos a la cola
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            // Eliminar y obtener el elemento al principio de la cola
+            int frontElement = queue.Dequeue();
+
+            // Obtener el elemento al principio de la cola sin eliminarlo
+            int peekedQueueElement = queue.Peek();
+        }
+    }
+}
